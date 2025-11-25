df46d3614cfa16d9b6b8af385c49755556b5747e
Wu-K'ung <sunxianfeng1115@gmail.com>
2025-11-25 16:59:22 +0800

    theme-app-guide

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { ref, computed } from 'vue'
+import Button from './components/Button.vue'
+
+const themePrefix = 'myapp'
+
+const themeObj = ref({
+  fontColor: '#dddddd',
+})
+const themeStr = computed(() => {
+  let str = ''
+
+  Object.entries(themeObj.value).forEach(([key, value]) => {
+    const CSSKey = `--${themePrefix}-${key.replace(/([A-Z])/g, '-$1').toLowerCase()}`
+    str += `${CSSKey}: ${value}; `
+  })
+
+  return str
+})
+</script>
+
+<template>
+  <div :style="themeStr">
+    <Button>按钮</Button>
+  </div>
+</template>
+
+<style>
+.button {
+  color: var(--myapp-font-color);
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <button :class="$style.button">
+    <slot></slot>
+  </button>
+</template>
+
+<style module>
+.button {
+  padding: 10px 20px;
+  font-size: 16px;
+  color: var(--myapp-font-color);
+  background-color: #006eda;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
